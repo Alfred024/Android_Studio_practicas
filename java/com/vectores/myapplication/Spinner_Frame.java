50f875edeaaf46fa8d966b539df0acc99bb036b5
Alfredo <21030761@itcelaya.edu.mx>
2023-05-22 12:16:41 -0600

    Xml(vacíos) de las demás prácticas

---
+++ java/com/vectores/myapplication/Spinner_Frame.java
@@ -0,0 +1,14 @@
+package com.vectores.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spinner_Frame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_frame);
+    }
+}
