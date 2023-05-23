6fda7ce8d3cceb8469da6cc8b93d424bec37ae97
Alfredo <21030761@itcelaya.edu.mx>
2023-05-23 10:26:07 -0600

    Img button demo terminado

--- java/com/vectores/myapplication/ImgButton_Frame.java
+++ java/com/vectores/myapplication/ImgButton_Frame.java
@@ -3,6 +3,7 @@ package com.vectores.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ImgButton_Frame extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ImgButton_Frame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img_button_frame);
+    }
+
+    public void imgBtn_info(){
+        Toast.makeText(this, "Práctica con img buttons para la materia de TAP", Toast.LENGTH_SHORT).show();
+    }
+
+    public void imgBtn_close(){
+        Toast.makeText(this, "Modal cerrado", Toast.LENGTH_SHORT).show();
     }
 }
