caac19861fffea2dcd686706adcc3f4f34876b2c
Alfredo <21030761@itcelaya.edu.mx>
2023-05-25 20:03:30 -0600

    Nueva versión pero no c qué cambios

--- java/com/vectores/myapplication/MainActivity.java
+++ java/com/vectores/myapplication/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
